5e7e6f54ea5ac39c75a811e471e79d1af9c0de23
Rubén Luengo Sánchez <rubluesan2@alu.edu.gva.es>
2026-01-23 00:49:58 +0100

    añadidas rutas propias a bank.php

--- routes/web.php
+++ routes/web.php
@@ -3,33 +3,15 @@
 use App\Http\Controllers\Settings;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\DefaultController;
-use App\Http\Controllers\CuentaController;
-use App\Http\Controllers\ClienteController;
-
 
 // Route::get('/', function () {
 //     return view('welcome');
 // })->name('home');
 
-Route::get('/', [DefaultController::class, 'home'])->name('home');
-
-// CUENTAS
-Route::get('/cuenta/list', [CuentaController::class, 'list'])->name('cuenta_list');
-Route::match(['get', 'post'], '/cuenta/new', [CuentaController::class, 'new'])->name('cuenta_new');
-Route::get('/cuenta/delete/{id}', [CuentaController::class, 'delete'])->name('cuenta_delete');
-Route::match(['get', 'post'], '/cuenta/edit/{id}', [CuentaController::class, 'edit'])->name('cuenta_edit');
-
-// CLIENTES
-Route::get('/cliente/list', [ClienteController::class, 'list'])->name('cliente_list');
-Route::match(['get', 'post'],'/cliente/new', [ClienteController::class, 'new'])->name('cliente_new');
-Route::get('/cliente/delete/{id}', [ClienteController::class, 'delete'])->name('cliente_delete');
-Route::match(['get', 'post'], '/cliente/edit/{id}', [ClienteController::class, 'edit'])->name('cliente_edit');
-
 // Dashboard
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
 //autenticacion
 Route::middleware(['auth'])->group(function () {
@@ -41,4 +23,5 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/appearance', [Settings\AppearanceController::class, 'edit'])->name('settings.appearance.edit');
 });
 
+require __DIR__.'/bank.php';
 require __DIR__.'/auth.php';
